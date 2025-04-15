fb1364710cbaddb8b041fdf5f377ae57c6bee7a3
fxckingtom <uyu99876a@gmail.com>
2025-04-15 20:40:37 +0800

    Remove deprecated Mongoose options

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use('/api', infoRoutes);
 
 mongoose.connect('mongodb+srv://uyu99876a:t7yuegnZKJhFra63@aw.4kg9zlm.mongodb.net/?retryWrites=true&w=majority&appName=aw', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => console.log('MongoDB 已連線'));
 
 app.listen(5000, () => console.log('伺服器運行於端口 5000'));
