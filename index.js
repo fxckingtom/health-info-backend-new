d6821f52743f999f411288086b4862ed9138d583
fxckingtom <uyu99876a@gmail.com>
2025-05-15 11:26:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose');
 const { OpenAI } = require('openai');
 
 // Models
-const HealthInfo = require('./seed');
-const HealthyRecipe = require('./seedHealthInfo');
+const HealthInfo = require('./models/HealthInfo');
+const HealthyRecipe = require('./models/HealthyRecipe');
 
 const app = express();
 
