b0268a69bb7fc2d5494a9501c61153ba0ed0c52d
fxckingtom <uyu99876a@gmail.com>
2025-05-19 15:18:16 +0800

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const HealthInfo = require('./models/HealthRecipe');
+const HealthInfo = require('./models/HealthyRecipe');
 
 async function seedRecipes() {
   try {
